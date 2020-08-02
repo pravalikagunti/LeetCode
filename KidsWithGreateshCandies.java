f2a6f8e112e90f376971b5374e1b5f4f6099bef2
pravalikagunti <36751620+pravalikagunti@users.noreply.github.com>
2020-08-02 01:25:03 -0700

    Add files via upload

---
+++ KidsWithGreateshCandies.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+/*Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+
+For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+
+ 
+
+Example 1:
+
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+Explanation: 
+Kid 1 has 2 candies and if he or she receives all extra candies (3) will have 5 candies --- the greatest number of candies among the kids. 
+Kid 2 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids. 
+Kid 3 has 5 candies and this is already the greatest number of candies among the kids. 
+Kid 4 has 1 candy and even if he or she receives all extra candies will only have 4 candies. 
+Kid 5 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids. 
+Example 2:
+
+Input: candies = [4,2,1,1,2], extraCandies = 1
+Output: [true,false,false,false,false] 
+Explanation: There is only 1 extra candy, therefore only kid 1 will have the greatest number of candies among the kids regardless of who takes the extra candy.
+Example 3:
+
+Input: candies = [12,1,12], extraCandies = 10
+Output: [true,false,true]
+ 
+
+Constraints:
+
+2 <= candies.length <= 100
+1 <= candies[i] <= 100
+1 <= extraCandies <= 50*/
+
+public class KidsWithGreateshCandies {
+
+	public static void main(String[] args) {
+		kidsWithCandies(new int[] {4,2,1,1,2},1);
+
+	}
+	 public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+		 int maxValue=0;
+		 List<Boolean> list=new ArrayList<>();
+		 for(int i:candies) {
+			 if(i>maxValue)
+				 maxValue=i;	
+			 Math.max(maxValue, i);
+		 }
+		// int maxValue=Arrays.stream(candies).max().getAsInt();
+		 for(int i:candies) {
+			 list.add(i+extraCandies>=maxValue?true:false);
+			/* if(i+extraCandies>=maxValue) {
+				 
+				 list.add(true);
+			 }
+			 else
+			 {
+				 list.add(false);
+			 }*/
+		 }
+		 return list;
+	        
+	    }
+	
+
+}
